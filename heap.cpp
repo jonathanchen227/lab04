8f660106529f7cbc805c67090fe9a7f032872d59
Jonathan Chen <jonathanchen227@ucsb.edu>
2026-02-20 23:17:36 -0800

    add

--- heap.cpp
+++ heap.cpp
@@ -1,36 +1,88 @@
 // heap.cpp
-// Diba Mirza
+// Jonathan Chen
 
 #include "heap.h"
 #include <iostream>
 using std::cout;
+#include <vector>
 
 // Builds a heap from the range [start, end) using the heapify algorithm
 // Should run in O(n) time
 Heap::Heap(std::vector<int>::iterator start, std::vector<int>::iterator end){
-
+	vector<int> vdata;
+	for ( auto i = start ; i != end; i ++ ) {
+		vdata.push_back(*i);
+	}
+	for ( int i = (vdata.size()-1)/2 ; i >= 0 ; i-- ) {
+		while ( true ) {
+			int index = i;
+			int left_child = 2*index + 1;
+			int right_child =2*index + 2;
+			if (left_child < n && vdata[left_child] < vdata[index] ) {
+				swap(vdata[index],vdata[left_child]);
+				index = left_child;
+			} 
+			if (right_child < n && vdata[right_child] < vdata[index]) {
+				swap(vdata[index],vdata[right_child]);
+				index = right_child;
+			}
+			else {
+				break;
+			}
+		}
+	}
 }
 
 // Pushes a value into the heap, then ensures
 // the heap is correctly arranged
 void Heap::push(int value){
-
+	vdata.push_back(value);
+	int index = vdata.size() - 1;
+	while ( vdata[index] < vdata[(index-1)/2] ) {
+		swap(vdata[index],vdata[(index-1)/2];
+		index = (index-1)/2;
+	} 
+	
 }
 
 // Pops the minimum value off the heap
 // (but does not return it), then ensures
 // the heap is correctly arranged
 void Heap::pop(){
-
+	if ( vdata.empty() ) {return;}
+	vdata.erase(vdata.begin());
+	for ( int i = (vdata.size() - 1)/2; i >= 0 ; i ++ ) {
+		while (true ) {
+			int index = i;
+			int left_child = 2*index + 1;
+			int right_child = 2*index + 2;
+			if ( left_child < n && vdata[index] > vdata[left_child] ) {
+				swap(vdata[index],vdata[left_child]);
+				index = left_child;
+			}
+			if ( right_child < n && vdata[index] > vdata[right_child] ) {
+				swap(vdata[index],vdata[right_child]);
+				index = right_child;
+			}
+			else {
+				break;
+			}
+		}
+	}	
 }
 
 // Returns the minimum element in the heap
 int Heap::top(){
-  return 0;
+  return vdata.begin();
 }
 
 // Returns true if the heap is empty, false otherwise
 bool Heap::empty(){
-  return true;
+  if ( vdata.empty() ) {
+	return true;
+ }
+ else {
+return false;
+}
 }
     
--- heap.h
+++ heap.h
@@ -1,6 +1,6 @@
 // heap.h
 // Binary heap tree header file for CS 24 lab
-// Diba Mirza
+// Jonathan Chen
 
 #ifndef HEAP_H
 #define HEAP_H
